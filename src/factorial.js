6eeea16be9279d04b55a09413ea456d4aac08425
Jonathan Pool <pool@stulta.com>
2017-05-31 16:36:47 -0700

    Revised control order and corrected tests. Created, tested, and debugged factorial.

---
+++ src/factorial.js
@@ -0,0 +1,30 @@
+/*
+  Function declaration for factorial and export statement making that
+  function the default export from this module.
+  Preconditions:
+    0. The argument count is 1.
+    1. Argument 0 is a positive finite integer.
+*/
+export default function factorial(number) {
+  // If the arguments are invalid:
+  if (
+    arguments.length !== 1
+    || typeof number !== 'number'
+    || number <= 0
+    || number === Infinity
+    || Math.ceil(number) !== Math.floor(number)
+  ) {
+    // Return a failure result.
+    return undefined;
+  }
+  // Otherwise:
+  else {
+    // Return the factorial of the number.
+    return Array(number - 1).fill(0).reduce(
+      (accumulator, currentValue, currentIndex) => {
+        return accumulator * (currentIndex + 2);
+      },
+      1
+    );
+  }
+}
