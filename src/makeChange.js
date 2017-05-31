ae958a45f53f87a1a035b672a19b7d8c2b4c1c8c
Jonathan Pool <pool@stulta.com>
2017-05-31 10:44:41 -0700

    Completed creation, testing, and debugging of makeChange.

---
+++ src/makeChange.js
@@ -0,0 +1,74 @@
+/*
+  Function declaration for makeChange and export statement making that
+  function the default export from this module.
+  Preconditions:
+    0. The argument count is 1.
+    1. Argument 0 is an object with properties price and amountGiven.
+    2. The value of price is a nonnegative integer.
+    3. The value of amountGiven is an integer no smaller than price.
+*/
+export default function makeChange(amounts) {
+  // If the arguments are valid:
+  if (
+    arguments.length === 1
+    && amounts !== undefined
+    && amounts !== null
+    && typeof amounts === 'object'
+    && amounts.hasOwnProperty('price')
+    && amounts.hasOwnProperty('amountGiven')
+    && Object.keys(amounts).length === 2
+    && typeof amounts.price === 'number'
+    && typeof amounts.amountGiven === 'number'
+    && amounts.price >= 0
+    && amounts.amountGiven >= amounts.price
+    && amounts.amountGiven !== Infinity
+    && Math.ceil(amounts.price) === Math.floor(amounts.price)
+    && Math.ceil(amounts.amountGiven) === Math.floor(amounts.amountGiven)
+  ) {
+    // Identify an array of names and values of denominations.
+    const coinTypes = [
+      ['dollars', 100],
+      ['half-dollars', 50],
+      ['quarters', 25],
+      ['dimes', 10],
+      ['nickels', 5],
+      ['pennies', 1]
+    ];
+    // Initialize the amount to be returned that has not yet been allocated.
+    let moreChange = amounts.amountGiven - amounts.price;
+    // Initialize an object containing the components of the change.
+    let changeCoins = {
+      'dollars': 0,
+      'half-dollars': 0,
+      'quarters': 0,
+      'dimes': 0,
+      'nickels': 0,
+      'pennies': 0
+    };
+    // For each denomination, in descending order of size:
+    for (let i = 0; i < coinTypes.length; i++) {
+      // If there is no more change to return:
+      if (moreChange === 0) {
+        // Stop calculating.
+        break;
+      }
+      // Identify its value.
+      const thisValue = coinTypes[i][1];
+      // Identify the count of coins of the denomination to return.
+      const thisCount = Math.floor(moreChange / thisValue);
+      // Identify the amount those coins constitute.
+      const thisAmount = thisCount * thisValue;
+      // Amend the object to specify the count of those coins.
+      changeCoins[coinTypes[i][0]] = thisCount;
+      // Amend the remaining amount to be returned.
+      moreChange -= thisAmount;
+    }
+    // Return the object.
+    return changeCoins;
+  }
+  // Otherwise, i.e. if the argument is invalid:
+  else {
+    // Return undefined.
+    return undefined
+  }
+}
---
+++ test/makeChange_test.js
@@ -0,0 +1,125 @@
+import { expect } from 'chai';
+import _makeChange from '../src/makeChange';
+
+describe('_makeChange', function() {
+
+  it('is a function', function() {
+    expect(_makeChange).to.be.a('function')
+  });
+
+  context('valid arguments', function() {
+
+    it(
+      'returns {dollars: 0, half-dollars: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 0} when price is 333 and amountGiven is 333', function() {
+        expect(_makeChange({price: 333, amountGiven: 333})).to.eql(
+          {
+            'dollars': 0,
+            'half-dollars': 0,
+            'quarters': 0,
+            'dimes': 0,
+            'nickels': 0,
+            'pennies': 0
+          }
+        );
+      }
+    );
+
+    it(
+      'returns {dollars: 3, half-dollars: 1, quarters: 1, dimes: 2, nickels: 0, pennies: 3} when price is 102 and amountGiven is 500', function() {
+        expect(_makeChange({price: 102, amountGiven: 500})).to.eql(
+          {
+            'dollars': 3,
+            'half-dollars': 1,
+            'quarters': 1,
+            'dimes': 2,
+            'nickels': 0,
+            'pennies': 3
+          }
+        );
+      }
+    );
+
+    it(
+      'returns {dollars: 0, half-dollars: 0, quarters: 0, dimes: 0, nickels: 0, pennies: 1} when price is 9999 and amountGiven is 10000', function() {
+        expect(_makeChange({price: 9999, amountGiven: 10000})).to.eql(
+          {
+            'dollars': 0,
+            'half-dollars': 0,
+            'quarters': 0,
+            'dimes': 0,
+            'nickels': 0,
+            'pennies': 1
+          }
+        );
+      }
+    );
+
+  })
+
+  context('invalid arguments', function() {
+
+    it('returns undefined when the argument is not an object', function() {
+      expect(_makeChange({price: 102, amountGiven: 500}, true)).to.be.undefined;
+    });
+
+    it('returns undefined when there are multiple arguments', function() {
+      expect(_makeChange('333, 500')).to.be.undefined;
+    });
+
+    it('returns undefined when given only price', function() {
+      expect(_makeChange({price: 333})).to.be.undefined;
+    });
+
+    it('returns undefined when given only amountGiven', function() {
+      expect(_makeChange({amountGiven: 500})).to.be.undefined;
+    });
+
+    it(
+      'returns undefined when given neither price nor amountGiven',
+      function() {
+        expect(_makeChange({})).to.be.undefined;
+      }
+    );
+
+    it('returns undefined when given an extraneous argument', function() {
+      expect(_makeChange({price: 333, amountGiven: 500, limit: 1000}))
+        .to.be.undefined;
+    });
+
+    it('returns undefined when there is a misnamed property', function() {
+      expect(_makeChange({price: 333, amountTendered: 500}))
+        .to.be.undefined;
+    });
+
+    it('returns undefined when price is not a number', function() {
+      expect(_makeChange({price: '333', amountGiven: 500})).to.be.undefined;
+    });
+
+    it('returns undefined when amountGiven is not a number', function() {
+      expect(_makeChange({price: 333, amountGiven: [500]})).to.be.undefined;
+    });
+
+    it('returns undefined when price is not an integer', function() {
+      expect(_makeChange({price: '333.5', amountGiven: 500})).to.be.undefined;
+    });
+
+    it('returns undefined when amountGiven is not an integer', function() {
+      expect(_makeChange({price: '333', amountGiven: 500.5})).to.be.undefined;
+    });
+
+    it('returns undefined when amountGiven < price', function() {
+      expect(_makeChange({price: 1333, amountGiven: 500})).to.be.undefined;
+    });
+
+    it('returns undefined when price is negative', function() {
+      expect(_makeChange({price: -40, amountGiven: 50})).to.be.undefined;
+    });
+
+    it('returns undefined when amountGiven is infinite', function() {
+      expect(_makeChange({price: 999995, amountGiven: Infinity}))
+        .to.be.undefined;
+    });
+
+  })
+
+})
