55dc41c84e9b0728865d07b8ef1e1fe86822ae24
Jonathan Pool <pool@stulta.com>
2017-05-31 15:18:14 -0700

    Created, tested, and debugged isPalindrome for an ASCII universe.

---
+++ src/isPalindrome.js
@@ -0,0 +1,29 @@
+/*
+  Function declaration for isPalindrome and export statement making that
+  function the default export from this module.
+  Preconditions:
+    0. The argument count is 1.
+    1. Argument 0 is a string.
+*/
+export default function isPalindrome(string) {
+  // If the arguments are valid:
+  if (
+    arguments.length === 1
+    && typeof string === 'string'
+  ) {
+    // Identify the string without disregarded characters and lower-cased.
+    const prunedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    // Identify its length.
+    const prunedLength = prunedString.length;
+    // Identify its halves, without any middle character.
+    const half0 = prunedString.slice(0, Math.floor(prunedLength / 2));
+    const half1 = prunedString.slice(Math.ceil(prunedLength / 2));
+    // Return whether the pruned string is a palindrome.
+    return half0 === half1.split('').reverse().join('');
+  }
+  // Otherwise, i.e. if the arguments are invalid:
+  else {
+    // Return undefined.
+    return undefined;
+  }
+}
---
+++ test/isPalindrome_test.js
@@ -0,0 +1,114 @@
+import { expect } from 'chai';
+import _isPalindrome from '../src/isPalindrome';
+
+describe('_isPalindrome', function() {
+
+  it('is a function', function() {
+    expect(_isPalindrome).to.be.a('function');
+  });
+
+  context('valid arguments', function() {
+
+    it(
+      'true when given an empty string', function() {
+        expect(_isPalindrome('')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a length-1 palindrome', function() {
+        expect(_isPalindrome('z')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a simple even-length palindrome', function() {
+        expect(_isPalindrome('anna')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a simple odd-length palindrome', function() {
+        expect(_isPalindrome('ana')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a cased even-length palindrome', function() {
+        expect(_isPalindrome('AnNa')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a cased odd-length palindrome', function() {
+        expect(_isPalindrome('Ana')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a punctuated palindrome', function() {
+        expect(_isPalindrome('a,.?b%&$#b()!@;a[]-/–/—')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a spaced palindrome', function() {
+        expect(_isPalindrome('a ba ')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a palindrome containing digits', function() {
+        expect(_isPalindrome('a22a')).to.be.true;
+      }
+    );
+
+    it(
+      'true when given a palindrome containing digits', function() {
+        expect(_isPalindrome('a22a')).to.be.true;
+      }
+    );
+
+    it(
+      'false when given a nonpalindrome', function() {
+        expect(_isPalindrome('airbnbra')).to.be.false;
+      }
+    );
+
+  });
+
+  context('invalid arguments', function() {
+
+    it(
+      'undefined when given no argument', function() {
+        expect(_isPalindrome()).to.be.undefined;
+      }
+    );
+
+    it(
+      'undefined when given multiple arguments', function() {
+        expect(_isPalindrome('aba', 'cdc')).to.be.undefined;
+      }
+    );
+
+    it(
+      'undefined when given a number', function() {
+        expect(_isPalindrome(565)).to.be.undefined;
+      }
+    );
+
+    it(
+      'undefined when given an array', function() {
+        expect(_isPalindrome(['monom'])).to.be.undefined;
+      }
+    );
+
+    it(
+      'undefined when given an object', function() {
+        expect(_isPalindrome({ecce: true})).to.be.undefined;
+      }
+    );
+
+  });
+
+});
